e589af5472037d90bb58f08aa27faa2211c3a429
Stephen O'Brien <wayofthepie@gmail.com>
2015-09-15 21:40:20 +0000

    Generalize test conduit, add some notes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Hacking
 Some hacking about with the conduit and json-stream library.
+
+## Working Example
+Some notes so I remember... Given the following json in a file **simple.json**:
+```json
+{
+    "a": {
+        "b" : "c"
+        "d" : {
+            "e": "f"
+        }
+    }
+}
+```
+I want to extract the value of **a** and **e**:
+```haskell
+-- | The parser would be as follows
+p :: Parser (T.Text, T.Text)
+p = (,) <$> "a" .: "b" .: string
+        <*> "a" .: "d" .: "e" .: string
+
+-- | And the conduit
+jsonC "simple.json" p
+
+-- | This gives
+[("c","f")]
+```
--- src/Main.hs
+++ src/Main.hs
@@ -4,27 +4,24 @@
 
 module Main where
 
-import Data.Aeson
 import qualified Data.ByteString.Char8 as BC
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
 import Data.JsonStream.Parser
-import qualified Data.Text as T
 import Control.Monad.Trans.Resource
 
-import Debug.Trace
 
 main :: IO ()
 main = putStrLn "hello world"
 
-jsonParse :: Show a => Parser a -> Conduit BC.ByteString (ResourceT IO) a
+jsonParse :: Parser a -> Conduit BC.ByteString (ResourceT IO) a
 jsonParse p = doParse $ parseOutput p
   where
-    parseOutput :: Show a => Parser a -> ParseOutput a
+    parseOutput :: Parser a -> ParseOutput a
     parseOutput p = runParser p
 
-    doParse :: Show a => ParseOutput a -> Conduit BC.ByteString (ResourceT IO) a
+    doParse :: ParseOutput a -> Conduit BC.ByteString (ResourceT IO) a
     doParse out = case out of
         ParseYield value newOutput  -> do
             yield $ value
@@ -36,11 +33,15 @@ jsonParse p = doParse $ parseOutput p
         ParseFailed err -> error err
 
 
--- | Function to test the above.
-jsonC :: String -> IO [(T.Text, Value)]
-jsonC fp = runResourceT $
-    let p = objectItems value :: Parser (T.Text, Value)
-    in CB.sourceFile fp
+-- | Function to test the jsonParse conduit.
+-- E.g
+-- @
+--  jsonC "example/test2.json" $
+--      (objectItems value :: Parser (T.Text, Value))
+-- @
+jsonC :: String -> Parser a -> IO [a]
+jsonC f p = runResourceT $
+    CB.sourceFile f
         $= CB.lines
         $$ jsonParse p
         $= CL.consume
